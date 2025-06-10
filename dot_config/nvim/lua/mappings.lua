7652a6691069e88b2f4fb9b6aa4f5478935d583c
anderson <andersonjoseph@mailfence.com>
2025-06-10 00:12:33 -0400

    add relative motions to jumplist

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -10,3 +10,16 @@ vim.keymap.set('n', '<leader>y', '"+y')
 vim.keymap.set('v', '<leader>y', '"+y')
 
 vim.keymap.set('n', '<leader>p', '"+p')
+
+local function jumplist_motion(key) -- adds relative motion to jumplist
+  return function()
+    local count = vim.v.count1
+    if count > 5 then  -- threshold of 5 lines
+      vim.cmd("normal! m'")  -- add to jump list
+    end
+    vim.cmd("normal! " .. count .. key)
+  end
+end
+
+vim.keymap.set('n', 'j', jumplist_motion('j'), { silent = true })
+vim.keymap.set('n', 'k', jumplist_motion('k'), { silent = true })
