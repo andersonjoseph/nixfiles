bb923600434dd994efbb932b4e27d3d416ea7623
anderson <andersonjoseph@mailfence.com>
2025-06-30 17:00:25 -0400

    disable supermaven when grinding leetcode

--- dot_config/nvim/lua/plugins/supermaven.lua
+++ dot_config/nvim/lua/plugins/supermaven.lua
@@ -2,5 +2,15 @@ return {
   "supermaven-inc/supermaven-nvim",
   config = function()
     require("supermaven-nvim").setup({})
+
+    vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+      pattern = { "*/leetcode/*" },
+      callback = function()
+	local api = require("supermaven-nvim.api")
+	
+	-- turn off inline suggestions in this buffer
+	api.toggle(false)
+      end,
+    })
   end,
 }
