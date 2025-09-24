a2d9b4df81dfdf7dbd2a477adf98b92c522356cc
andersonjoseph <andersonjoseph@mailfence.com>
2025-09-24 11:57:03 -0400

    add secondary hard drive

--- hosts/almazrah/default.nix
+++ hosts/almazrah/default.nix
@@ -14,5 +14,11 @@
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia.open = false;
 
+  fileSystems."/mnt/dos" = {
+    device = "/dev/disk/by-uuid/479c69de-544c-4061-8aa3-666a57e71d4c";
+    fsType = "ext4";
+    options = [ "defaults" "nofail" ];
+  };
+
   networking.hostName = "almazrah";
 }
