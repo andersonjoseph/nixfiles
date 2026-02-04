547a95f56d790a96f32ea45a368683a88c8d6d72
anderson <andersonjoseph@mailfence.com>
2026-02-04 12:07:43 -0400

    disable opencode on editor

--- home/nvim/lua/plugins/opencode.lua
+++ home/nvim/lua/plugins/opencode.lua
@@ -9,11 +9,7 @@ return {
     -- Recommended/example keymaps.
     vim.keymap.set("n", "<C-p>", function() require("opencode").ask("@buffer: ", { submit = true }) end, { desc = "Ask opencode…" })
     vim.keymap.set("v", "<C-p>", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" })
-    vim.keymap.set({ "n", "t" }, "<S-C-p>", function() require("opencode").toggle() end,                          { desc = "Toggle opencode" })
 
     vim.keymap.set({ "n", "x" }, "<C-.>", function() require("opencode").select() end,                          { desc = "Execute opencode action…" })
-
-    vim.keymap.set("n", "<S-C-u>", function() require("opencode").command("session.half.page.up") end,   { desc = "Scroll opencode up" })
-    vim.keymap.set("n", "<S-C-d>", function() require("opencode").command("session.half.page.down") end, { desc = "Scroll opencode down" })
   end,
 }
