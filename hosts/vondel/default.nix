42e52e88917936118101c7fb3d85a90d0a65d8da
anderson <andersonjoseph@mailfence.com>
2026-02-04 22:12:57 -0400

    add docker to vondel

--- hosts/vondel/default.nix
+++ hosts/vondel/default.nix
@@ -38,11 +38,16 @@
       "networkmanager"
       "wheel"
       "nordvpn"
+      "docker"
     ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID4RQrSTll/Ugui6ty+c6XvBCPYHT9OMm7F1K4KeTiCC almazrah->vondel"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC94m5sA5JJQ+n4UYoUCiT1YNYxsmTYFjk1wYupVesna ashika->vondel"
     ];
+  };
+
+  virtualisation.docker = {
+    enable = true;
   };
 
   services.fail2ban.enable = true;
