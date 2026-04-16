d952fda3dd7bea07c8b78b1ffb2e00fff6676f7c
anderson <andersonjoseph@mailfence.com>
2026-04-16 16:56:24 -0400

    Revert "add pi environment"
    This reverts commit 43fbb6e9b024f10d9d4fbf3362ecdf1785fe6b04.

--- home/default.nix
+++ home/default.nix
@@ -189,14 +189,6 @@
         home.file.".config/nvim" = {
           source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/configuration/home/nvim";
         };
-
-        home.file.".pi/agent/AGENTS.md" = {
-          source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/configuration/home/pi/agent/AGENTS.md";
-        };
-
-        home.file.".pi/agent/prompts" = {
-          source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/configuration/home/pi/agent/prompts";
-        };
       };
   };
 }
